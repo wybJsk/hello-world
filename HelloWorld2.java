31c18b9a460d1cec30742ea1dc37092fbff48d2b
Wangyb <18638939562@163.com>
2019-11-29 20:30:39 +0800

    helloworld2

---
+++ HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		system.out.println("helloworld");
+		system.out.println("1");
+		system.out.println("helloworld");
+		system.out.println("2");
+	}
+}
